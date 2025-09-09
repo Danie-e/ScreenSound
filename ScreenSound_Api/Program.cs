49139ca1042e0094a31d058d7d84dcdf0e4034c2
Danie-e <danielaeloisa1@gmail.com>
2025-09-09 11:01:28 -0300

    Utilizando Swagger para documentar a api.

--- ScreenSound_Api/Program.cs
+++ ScreenSound_Api/Program.cs
@@ -10,11 +10,17 @@ builder.Services.AddTransient<DAL<Banda>>();
 builder.Services.AddTransient<DAL<Album>>();
 builder.Services.AddTransient<DAL<Musica>>();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
 app.AddEndpointsBandas();
 app.AddEndpointsAlbuns();
 app.AddEndpointsMusicas();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
