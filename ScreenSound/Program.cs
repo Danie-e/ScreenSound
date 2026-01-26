a17e4d978b08dd257c054da017abf5d608e6d8da
Danie-e <danielaeloisa1@gmail.com>
2026-01-26 10:39:58 -0300

    Criando menu com opções e regatando valor digitado pelo usuário.

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -5,7 +5,7 @@
         string mensagemDeBoasVindas = "Bem vindo ao Screen Sound!";
 
         ExibirMensagemDeBoasVindas();
-
+        ExibirOpcoesDoMenu();
 
 
         void ExibirMensagemDeBoasVindas()
@@ -20,6 +20,20 @@
 ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
 ");
             Console.WriteLine(mensagemDeBoasVindas);
+        }
+
+        void ExibirOpcoesDoMenu()
+        {
+            Console.WriteLine("\nEscolha uma das opções abaixo:");
+            Console.WriteLine("1 - Cadastrar banda");
+            Console.WriteLine("2 - Listar bandas");
+            Console.WriteLine("3 - Avaliar bandas");
+            Console.WriteLine("4 - Exibir média  bandas");
+            Console.WriteLine("0 - Sair");
+
+            Console.Write("Digite a opção escolhida: ");
+            int opcaoEscolhida = int.Parse(Console.ReadLine()!);
+
         }
     }
 }
