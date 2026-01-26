09a7da7488fe9bafb616aa491b60629ffa5d59da
Danie-e <danielaeloisa1@gmail.com>
2026-01-26 10:04:33 -0300

    Criando mensagem de boas-vindas ao programa.

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string mensagemDeBoasVindas = "Bem vindo ao Screen Sound!";
+        Console.WriteLine("**************************");
+        Console.WriteLine(mensagemDeBoasVindas);
+        Console.WriteLine("**************************");
+
+    }
+}
