890ae332daabbb5e207aadbc6ed5069c9f841a4f
Danie-e <danielaeloisa1@gmail.com>
2026-02-04 17:49:08 -0300

    Criação da classe Album para armazenar lista de músicas.

---
+++ ScreenSound/Models/Album.cs
@@ -0,0 +1,12 @@
+﻿namespace ScreenSound.Models;
+
+internal class Album
+{
+    public string Nome { get; set; }
+    private List<Musica> ListaMusicas { get; set; }
+
+    public void AdicionarMusica(Musica musica)
+    {
+        ListaMusicas.Add(musica);
+    }
+}
