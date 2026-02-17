241d8465ba71daa367dc63bb38bb11f7e613ceb5
Danie-e <danielaeloisa1@gmail.com>
2026-02-17 11:36:43 -0300

    Adaptando a classe para receber a resposta da api.

--- ScreenSound/models/Musica.cs
+++ ScreenSound/models/Musica.cs
@@ -1,16 +1,29 @@
-﻿namespace ScreenSound.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace ScreenSound.Models;
 
 internal class Musica
 {
+    public Musica() { }
     public Musica(string nome, Banda banda)
     {
         Nome = nome;
         Artista = banda;
     }
 
-    public readonly string Nome  = string.Empty;
-    public Banda Artista { get; set; }
+    [JsonPropertyName("song")]
+    public readonly string Nome = string.Empty;
+
+    [JsonPropertyName("artist")]
+    public string NomeArtista { get; set; }
+
+    [JsonPropertyName("duration_ms")]
     public int Duracao { get; set; } = 0;
+
+    [JsonPropertyName("genre")]
+    public string Genero { get; set; }
+
+    public Banda Artista { get; set; }
     public bool Disponivel { get; set; } = true;
     public string DescricaoResumida => $"A musica {Nome} pertence ao artista {Artista.Nome}.";
 
@@ -18,8 +31,9 @@ internal class Musica
     public void ExibirFichaTecnica()
     {
         Console.WriteLine($"Nome: {Nome}");
-        Console.WriteLine($"Artista: {Artista.Nome}");
-        Console.WriteLine($"Duração: {Duracao} minutos");
+        Console.WriteLine($"Artista: {NomeArtista}");
+        Console.WriteLine($"Genero: {Genero}");
+        Console.WriteLine($"Duração: {Duracao/100} minutos");
         Console.WriteLine($"Disponível: {(Disponivel ? "Sim" : "Não")}");
     }
 }
